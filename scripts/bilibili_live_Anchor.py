d228f619d4f0958c761b705b7e3b6175a7048ec5
lanan <2436725966@qq.com>
2025-05-31 21:44:47 +0800

    添加跳转直播管理网页按钮

--- scripts/bilibili_live_Anchor.py
+++ scripts/bilibili_live_Anchor.py
@@ -6318,40 +6318,12 @@ def script_description():
     """
     if not globalVariableOfData.networkConnectionStatus:
         return "<font color=yellow>网络不可用</font>"
-
     t = ('<html lang="zh-CN"><body><pre>\
-本插件基于python3<br>\
-    如果未安装python3，请前往<br>\
-        <a href="https://www.python.org/">python官网</a><br>\
-        或者<br>\
-        <a href="https://python.p2hp.com/">python中文网 官网</a>下载安装<br>\
-        不同操作系统请查看<br>\
-            菜鸟教程<a href="https://www.runoob.com/python3/python3-install.html">Python3 环境搭建</a><br>\
-<font color=yellow>!脚本路径中尽量不要有中文</font><br>\
-<font color=green size=4>请在认为完成操作后点击<font color="white" size=5>⟳</font>重新载入插件</font><br>\
-配置cookie：<br>\
-<font color=yellow>！请看着脚本日志操作</font><br>\
-扫描配置cookie请 提前增加<br>\
-   脚本日志窗口 宽高<br>\
-手动配置cookie请前往<br>\
-   <a href="https://link.bilibili.com/p/center/index#/my-room/start-live">B站直播设置后台</a> 使用<br>\
-       浏览器的开发人员工具获取cookie<br><br>\
-<font color="#ee4343">【cookie！为账号的{极重要}的隐私信息!】</font><br>\
-<font color="#ee4343">【！不要泄露给他人!】</font><br>\
-<br>\
-如果报错：<br>\
-   请关闭梯子和加速器<br>\
-   Windows请尝试使用<font color="#ee4343">管理员</font>权限运行obs<br>\
-   其它系统请联系开发者<br>\
-</pre></body></html>')
-    t = ('<html lang="zh-CN"><body><pre>\
-本插件基于<font color="#ee4343" size=5>python3.10</font><br>\
+本插件建议使用<font color="#ee4343" size=5>python3.10</font><br>\
 <font color=yellow>!脚本路径中尽量不要有中文</font><br>\
 <font color="white" size=5>⟳</font><font color=green size=4>为重新载入插件按钮</font><br>\
-如果报错：<br>\
-   关闭梯子或加速器<br>\
-   Windows请尝试使用<font color="#ee4343">管理员权限</font>运行obs<br>\
-   其它问题请前往<a href="https://github.com/lanyangyin/OBSscripts-bilibili-live/issues">Github</a>提问<br>\
+使用<font color="#ee4343">管理员权限</font>运行obs<br>\
+其它问题请前往<a href="https://github.com/lanyangyin/OBSscripts-bilibili-live/issues">Github</a>或者<a href="https://message.bilibili.com/#/whisper/mid143474500">B站</a>提问\
 </pre></body></html>')
     return t
 
